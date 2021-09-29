1cddeaff9d9471fa5eae12ec7ebefdd48fb319c0
Mehul Mohan <mehulmpt@gmail.com>
2021-09-29 18:32:59 +0530

    lab

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// write your code here
+// convert this code to express based code
+const http = require('http')
+
+const requestListener = function (req, res) {
+	res.writeHead(200)
+	res.end('Hello, World!')
+}
+
+const server = http.createServer(requestListener)
+server.listen(process.env.PUBLIC_PORT)
